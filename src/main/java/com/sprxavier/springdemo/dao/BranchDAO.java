a5fffe41e1505bb1b473da65ce50973d3804d200
Xavier <1059044790@qq.com>
2022-03-06 21:15:06 +0800

    Add SelectById-->branch DAO

--- src/main/java/com/sprxavier/springdemo/dao/BranchDAO.java
+++ src/main/java/com/sprxavier/springdemo/dao/BranchDAO.java
@@ -2,6 +2,7 @@ package com.sprxavier.springdemo.dao;
 
 import com.sprxavier.springdemo.pojo.branch;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -18,5 +19,8 @@ public interface BranchDAO {
 
     @Select("SELECT * FROM branch;")
     public List<branch> selectAll();
+
+    @Select("SELECT * FROM branch WHERE branch_id = #{branchId};")
+    public branch selectById(@Param("branchId") int id);
 
 }
